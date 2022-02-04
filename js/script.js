112b147f4f77c1611440f3ff577e8120deb0ab23
AhriannaDaniels <86747048+AhriannaDaniels@users.noreply.github.com>
2022-02-04 11:19:20 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -169,28 +169,31 @@ function collaspeMenu() {
         menuState = false;
 }
 
-// for(i=1; i<101;i++){
-//     var xAxis = Math.floor(Math.random() * 100) + 1 + '%';
-//     var yAxis = "-" + Math.floor(Math.random() * 100) + 1 + '%';
-//     var element = document.createElement("div")
-//     element.classList.add('star');
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 22, 2022 00:00:00").getTime();
 
-//     element.style.left = xAxis;
-//     element.style.bottom = yAxis;
-//     starContainer.appendChild(element)
-    
-//     staryNight(element, "star")
-// }
+// Update the count down every 1 second
+var x = setInterval(function() {
 
+  // Get today's date and time
+  var now = new Date().getTime();
 
-// for(i=1; i<51;i++){
-//     var xAxis = Math.floor(Math.random() * 100) + 1 + '%';
-//     var yAxis = "-" + Math.floor(Math.random() * 100) + 1 + '%';
-//     var element = document.createElement("div")
-//     element.classList.add('bigstar');
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
 
-//     element.style.left = xAxis;
-//     element.style.bottom = yAxis;
-//     bigStarContainer.appendChild(element)
-//     staryNight(element, "star")
-// }
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = "Countdown </br>" +days + "D " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "WE LAUNCHED!";
+  }
+}, 1000);
